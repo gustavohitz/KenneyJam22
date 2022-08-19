ccd446f3e27d3a9597d4b02337117cfe85c22c78
Gustavo Hitzschky <gust.campos@gmail.com>
2022-08-19 13:14:48 -0300

    Player script basic movement

---
+++ Assets/_Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    
+    public Rigidbody2D rb2D;
+    public float speed = 4f;
+    private float _inputAxis;
+    private float _inputVertical;
+
+    void Awake() {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+    void Update() {
+        Move();
+    }
+
+    void FixedUpdate() {
+        rb2D.velocity = new Vector2(_inputAxis * speed, _inputVertical * speed);
+    }
+
+    void Move() {
+        _inputAxis = Input.GetAxis("Horizontal");
+        _inputVertical = Input.GetAxis("Vertical");
+        if(_inputAxis > 0) {
+            transform.eulerAngles = new Vector2(0f, 0f);
+        }
+        
+        if(_inputAxis < 0) {
+            transform.eulerAngles = new Vector2(0f, 180f);
+        }
+    }
+
+}
